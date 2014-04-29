831e64b01ac442508ed6193c99112434907f57eb
Dan Leehr <dan.leehr@nescent.org>
2014-04-29 15:38:56 -0400

    Include missing carrot2 pom

--- ansible-dryad/roles/dryad_app/files/install_missing_poms.sh
+++ ansible-dryad/roles/dryad_app/files/install_missing_poms.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Per http://wiki.datadryad.org/How_To_Install_Dryad#Installation_issues_with_maven
 mvn install:install-file -Dfile=discovery-solr-provider-0.9.4-SNAPSHOT.jar  -DgroupId=org.dspace.discovery -DartifactId=discovery-solr-provider -Dversion=0.9.4-SNAPSHOT -Dpackaging=jar
 mvn install:install-file -Dfile=dspace-solr-solrj-1.4.0.1-SNAPSHOT.jar  -DgroupId=org.dspace.dependencies.solr -DartifactId=dspace-solr-solrj -Dversion=1.4.0.1-SNAPSHOT -Dpackaging=jar
 mvn install:install-file -Dfile=discovery-xmlui-block-0.9.4-SNAPSHOT.jar  -DgroupId=org.dspace.discovery -DartifactId=discovery-xmlui-block -Dversion=0.9.4-SNAPSHOT -Dpackaging=jar
+
+# Carrot2 is missing, discovered on fresh ubuntu install
+cd /tmp
+mvn install:install-file -DgroupId=org.carrot2 -DartifactId=carrot2-mini -Dversion=3.1.0 -Dpackaging=jar -Dfile=carrot2-mini-3.1.0.jar
