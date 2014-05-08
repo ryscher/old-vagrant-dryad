011730c8a9ceb5904174dd85e68cbbef34ba678e
Dan Leehr <dan.leehr@nescent.org>
2014-05-08 13:40:45 -0400

    Delete setenv.sh

--- ansible-dryad/roles/dryad_app/files/setenv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-export CATALINA_HOME=/var/lib/tomcat6
-export JAVA_HOME="/usr/lib/jvm/java-6-oracle"
-export JAVA_OPTS="-Xmx4096m -XX:+CMSClassUnloadingEnabled -XX:PermSize=512M -XX:MaxPermSize=512M"
