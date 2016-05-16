51ea1b78c36c973b39de11f11fbed0e84caf2265
Ryan Scherle <ryan@scherle.org>
2016-05-16 19:23:42 -0400

    Install driver for postgres 9.4

--- ansible-dryad/roles/dryad_app/files/install_missing_poms.sh
+++ ansible-dryad/roles/dryad_app/files/install_missing_poms.sh
@@ -4,6 +4,7 @@
 mvn install:install-file -Dfile=discovery-solr-provider-0.9.4-SNAPSHOT.jar  -DgroupId=org.dspace.discovery -DartifactId=discovery-solr-provider -Dversion=0.9.4-SNAPSHOT -Dpackaging=jar
 mvn install:install-file -Dfile=dspace-solr-solrj-1.4.0.1-SNAPSHOT.jar  -DgroupId=org.dspace.dependencies.solr -DartifactId=dspace-solr-solrj -Dversion=1.4.0.1-SNAPSHOT -Dpackaging=jar
 mvn install:install-file -Dfile=discovery-xmlui-block-0.9.4-SNAPSHOT.jar  -DgroupId=org.dspace.discovery -DartifactId=discovery-xmlui-block -Dversion=0.9.4-SNAPSHOT -Dpackaging=jar
+mvn install:install-file -DgroupId=postgresql -DartifactId=postgresql -Dversion=9.4-1206-jdbc41 -Dpackaging=jar -Dfile=../postgres/postgresql-9.4-1206-jdbc41.jar
 
 # Carrot2 is missing, discovered on fresh ubuntu install
 cd /tmp
