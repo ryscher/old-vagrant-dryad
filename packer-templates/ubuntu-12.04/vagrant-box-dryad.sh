9a1f34ba8c7f2413274f1cd766840e7ac35416be
daisie_local <daisieh@gmail.com>
2016-08-19 00:50:10 -0700

    fix paths

--- packer-templates/ubuntu-12.04/vagrant-box-dryad.sh
+++ packer-templates/ubuntu-12.04/vagrant-box-dryad.sh
@@ -2,5 +2,6 @@
 #
 
 packer build -only=virtualbox-iso -parallel=true template.json
-vagrant box add ubuntu-12-04-x64-virtualbox.box --name dryad-ubuntu-12-04
+cd ../..
+vagrant box add packer-templates/ubuntu-12.04/ubuntu-12-04-x64-virtualbox.box --name dryad-ubuntu-12-04
 
